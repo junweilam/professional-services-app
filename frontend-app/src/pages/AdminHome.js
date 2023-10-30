c4f279188c89adbe82ff536d4901bd6575902b7c
jianweiiii <jianweii97@hotmail.com>
2023-10-30 20:00:21 +0800

    added logout api

--- frontend-app/src/pages/AdminHome.js
+++ frontend-app/src/pages/AdminHome.js
@@ -1,13 +1,26 @@
-import axios from "axios";
 import React from "react";
 import { handleAuth } from "../features/apiCalls";
+import { logOut } from "../features/apiCalls";
 
 const AdminHome = () => {
-    const handleLogout = () => {
-        // Clear the token from localStorage
-        localStorage.removeItem('token');
-        // Redirect the user to the login page
-        window.location.href = '/login';
+    const handleLogout = async () => {
+        let token = {token: localStorage.getItem("token")}
+        
+        
+        try{
+            const response = await logOut(token)
+            console.log(response)
+            if(response.message == "Logout successful"){
+                // Clear the token from localStorage
+                localStorage.removeItem('token');
+                // Redirect the user to the login page
+                 window.location.href = '/signin';
+            }
+
+        }catch(err){
+            console.log("err in logout", err)
+        }
+
     }
 
     const handleClick = async () => {
