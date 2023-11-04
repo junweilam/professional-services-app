3f2c4b2b5d0159b8944ab706a6456cea59e60950
Nicholas Quek <nicholasquek98@gmail.com>
2023-11-04 11:52:33 +0800

    added back to home after checkout successful

--- frontend-app/src/component/CheckoutSucess.js
+++ frontend-app/src/component/CheckoutSucess.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CheckoutSuccess = () => {
   return (
@@ -6,6 +7,11 @@ const CheckoutSuccess = () => {
       <div className="bg-white p-8 rounded-lg shadow-md text-center">
         <h2 className="text-2xl font-semibold mb-4">Checkout Successful</h2>
         <p className="text-gray-600">Thank you for your order. Your payment was successful.</p>
+        <Link to="/homepage"> 
+          <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 mt-4">
+            Go Back to Homepage
+          </button>
+        </Link>
       </div>
     </div>
   );
