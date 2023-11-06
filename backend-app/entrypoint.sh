91614736a182f08d55bbde54c5d975c64496672b
jianweiiii <jianweii97@hotmail.com>
2023-11-06 16:31:00 +0800

    fixed docker config for QA to access

--- backend-app/entrypoint.sh
+++ backend-app/entrypoint.sh
@@ -1,23 +1,2 @@
-#!/bin/bash
-
-# Start your Node.js application
-node server.js &
-
-
-# # Wait for the MySQL server to be ready
-# until mysql -hsql-server -uusers -ppw123123 -e 'SELECT 1'; do
-#   echo 'Waiting for MySQL to be ready...'
-#   sleep 5
-# done
-
-# # Run the seeding SQL script
-echo 'Running database seeding...'
-
-mysql -hsql-server -uroot -ppw123123 < seed.sql
-
-echo 'Database seeder completed...'
-
-# Start the MySQL server
-exec "$@"
-
-tail -f /dev/null
+#!/bin/sh
+node /app/server.js 
